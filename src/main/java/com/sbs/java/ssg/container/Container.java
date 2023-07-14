0e375fbe67b3cc63e59fcdce0c771e57ba6dc9be
dbwnstjs4862 <dbwnstjs4862@gmail.com>
2023-07-14 11:24:17 +0900

    DB커넥션 추가 후 DB설정까지

---
+++ src/main/java/com/sbs/java/ssg/container/Container.java
@@ -0,0 +1,44 @@
+package com.sbs.java.ssg.container;
+
+import com.sbs.java.ssg.controller.Session;
+import com.sbs.java.ssg.dao.ArticleDao;
+import com.sbs.java.ssg.dao.MemberDao;
+import com.sbs.java.ssg.db.DBConnection;
+import com.sbs.java.ssg.service.ArticleService;
+import com.sbs.java.ssg.service.ExportService;
+import com.sbs.java.ssg.service.MemberService;
+
+public class Container {
+	public static Session session;
+	public static DBConnection dbConnection;
+	public static ArticleDao articleDao;
+	public static MemberDao memberDao;
+	public static ArticleService articleService;
+	public static MemberService memberService;
+	public static ExportService exportService;
+	
+	static {
+		articleDao = new ArticleDao();
+		memberDao = new MemberDao();
+		articleService = new ArticleService();
+		memberService = new MemberService();
+		exportService = new ExportService();
+	}
+	
+	public static Session getSession() {
+		if ( session == null ) {
+			session = new Session();
+		}
+		
+		return session;
+	}
+	
+	public static DBConnection getDBConnection() {
+		if ( dbConnection == null ) {
+			dbConnection = new DBConnection();
+		}
+		
+		return dbConnection;
+	}
+
+}
